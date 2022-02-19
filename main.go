bfc6856b69c08d46d2112628c615119078be7291
aphrem_thomas <aphremthomas@gmail.com>
2022-02-19 22:10:05 +0530

    added arrays and slices

--- main.go
+++ main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("howdi partner")
+	var counter int = 7
+	fmt.Println("howdi partner", counter)
+	var arrayDemo = [20]string{"Tom", "Jerry"} //fixed lenght ones are called arrays
+	fmt.Println("array", arrayDemo)
+	arrayDemo[3] = "harry"
+	fmt.Println("array is ", arrayDemo)
+
+	var sliceDemo []string
+	sliceDemo = append(sliceDemo, "Aphrem")
+	fmt.Println("sliceDemo after append", sliceDemo)
+	for i := 0; i < len(arrayDemo); i++ { //for loop demo
+		fmt.Println(arrayDemo[i])
+	}
+
 }
